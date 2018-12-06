df772f834e29034fa52c7f5cef7da7f5c0b4ee7a
mistermik <mferrari@microsoft.com>
2018-12-06 08:40:33 -0600

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        MISTERMIK!!!!!!!!ddd
+        MISTERMIK!!!!!!!!
       </div>
       <div class="col-lg-6">
       </div>
