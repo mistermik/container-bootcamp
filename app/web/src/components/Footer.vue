90de075b1848569aabdfa7808bdbfe861be488ee
mistermik <mferrari@microsoft.com>
2018-12-06 00:20:13 -0600

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team MISTERMIK!!!!!!!!!!!!!!!!!!!!!
+        Azure Global Blackbelt Team MISTERMIK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       </div>
       <div class="col-lg-6">
       </div>
